403121a0a5c12d2b681df13559902a0e8238d33f
William Keller <william.keller@csuglobal.edu>
2025-03-23 22:03:59 -0400

    adding initial files to repo

---
+++ src/Module1/Module1.java
@@ -0,0 +1,37 @@
+package Module1;
+
+
+/* 
+ * Pseudocode:
+ * Set First name
+ * Set Last name
+ * Set Street address
+ * Set City
+ * Set Zipcode
+ * 
+ * Print First name
+ * Print Lasts name
+ * Print Street address
+ * Print City
+ * Print Zipcode
+ * 
+ */
+public class Module1 {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Eric";
+		String lastName = "Brown";
+		String Address = "214 Playground Drive";
+		String City = "West Reading";
+		String zipcode = "47532";
+		
+		System.out.println("The user's First Name is " + firstName);
+		System.out.println("The user's Last Name is " + lastName);
+		System.out.println("The user's Street address is " + Address);
+		System.out.println("The user's City is " + City);
+		System.out.println("The user's Zip code is " + zipcode);
+		
+	}
+
+}
